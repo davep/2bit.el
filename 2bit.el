be9a8320ac327409459db59c34cbaa7f3ff2c83f
Dave Pearson <davep@davep.org>
2020-08-29 15:23:56 +0100

    :books: Point the reader at the README in the header of the file

--- 2bit.el
+++ 2bit.el
@@ -23,7 +23,8 @@
 ;;; Commentary:
 ;;
 ;; 2bit.el provides functions and commands that help extract data from 2bit
-;; files.
+;; files. Please see https://github.com/davep/2bit.el/blob/main/README.md
+;; for more details on what this package provides.
 
 ;;; Code:
 
